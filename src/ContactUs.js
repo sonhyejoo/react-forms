3c699229e7a0ffaa0f18b6d7cff9ee06af081a21
Son Hyejoo <seongwoo.cho@gmail.com>
2023-03-31 21:03:35 +0000

    select done in react

--- src/ContactUs.js
+++ src/ContactUs.js
@@ -5,12 +5,14 @@ function ContactUs() {
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
   const [comments, setComments] = useState("");
+  const [phoneType, setPhoneType] = useState("");
   const onSubmit = (e) => {
     e.preventDefault();
     const contactUsInfo = {
       name,
       email,
       phone,
+      phoneType,
       comments,
       submittedOn: new Date(),
     };
@@ -18,6 +20,7 @@ function ContactUs() {
     setName("");
     setEmail("");
     setPhone("");
+    setPhoneType("");
     setComments("");
   };
 
@@ -51,6 +54,18 @@ function ContactUs() {
             onChange={(e) => setPhone(e.target.value)}
             value={phone}
           />
+          <select
+            name="phoneType"
+            onChange={(e) => setPhoneType(e.target.value)}
+            value={phoneType}
+          >
+            <option value="" disabled>
+              Select a phone type...
+            </option>
+            <option value="Home">Home</option>
+            <option value="Work">Work</option>
+            <option value="Mobile">Mobile</option>
+          </select>
         </div>
         <div>
           <label htmlFor="comments">Comments: </label>
